d4a3f5177090337df63507cb79d5136f82855ebd
Stanislav Sinelnikov <siniy409@gmail.com>
2022-02-17 21:07:41 +0300

    first commit

---
+++ src/main/java/lesson2/HomeWorkApp2.java
@@ -0,0 +1,38 @@
+package lesson2;
+
+class HomeWorkApp2 {
+
+    public static void main(String[] args) {
+        System.out.println(is10To20(12, -4));
+        System.out.println(is10To20(10, 3));
+        System.out.println(is10To20(15, 7));
+
+        printPositiveOrNegative(1);
+        printPositiveOrNegative(0);
+        printPositiveOrNegative(-1);
+
+        System.out.println(trueOrFalseNumber(-1));
+        System.out.println(trueOrFalseNumber(0));
+        System.out.println(trueOrFalseNumber(1));
+
+        repeatString("Lesson 2", 4);
+    }
+
+    static boolean is10To20(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
+    }
+
+    static void printPositiveOrNegative(int a) {
+        System.out.println(a >= 0? "Positive" : "Negative");
+    }
+
+    static boolean trueOrFalseNumber(int a) {
+        return a < 0;
+    }
+
+    static void repeatString(String s, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(s);
+        }
+    }
+}
