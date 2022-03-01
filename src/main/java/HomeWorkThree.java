b4ca43eb75adde333ca3eeeb61a6c2598601f9ba
siny409 <99689456+siny409@users.noreply.github.com>
2022-03-01 20:37:46 +0300

    Update HomeWorkThree.java

--- src/main/java/HomeWorkThree.java
+++ src/main/java/HomeWorkThree.java
@@ -61,19 +61,20 @@ public class HomeWorkThree {
         return arr;
     }
 
-    static void minMax(){
+    static void minMax() {
         int[] mas = {7, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 5};
         int min = mas[0], max = mas[0], i, imin = 0, imax = 0;
             for(i = 0; i != mas.length; i++) {
-                if(mas[i] < min){
+                if(mas[i] < min) {
                     min = mas[i];
                     imin = i;
                 }
-                if(mas[i] > max){
+                if(mas[i] > max) {
                     max = mas[i];
                     imax = i;
                 }
-                System.out.println("max = "+max+" min = "+min);
-                System.out.println("imax = "+imax+" imin = "+imin);}
+                System.out.println("max = " + max + " min = " + min);
+                System.out.println("imax = " + imax + " imin = " + imin);
+        }
     }
 }
